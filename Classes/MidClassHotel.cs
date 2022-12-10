ac042529a9be5bd5232e730f6f1fbcccdf43ea25
KostaUrumov <koceurumov@gmail.com>
2022-12-10 10:15:06 +0000

    add

--- Classes/MidClassHotel.cs
+++ Classes/MidClassHotel.cs
@@ -112,6 +112,15 @@ namespace BookingSystem.Classes
         public override void Reserve(People person)
         {
             reservations.Add(person);
+            Room room = new Room(person.Start, person.End);
+            if (person.RoomType == "1")
+            {
+                luxury.Add(room);
+            }
+            if (person.RoomType == "2")
+            {
+                roomsD.Add(room); 
+            }
 
         }
 
