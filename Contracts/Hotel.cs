1eacb92de3d757f4cd0932cb9a146ab5783e2be5
KostaUrumov <koceurumov@gmail.com>
2022-12-12 12:57:37 +0000

    interface

---
+++ Contracts/Hotel.cs
@@ -0,0 +1,38 @@
+﻿using BookingSystem.Classes;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace BookingSystem.Contracts
+{
+    public abstract class IHotel
+    {
+
+        public IHotel(string name, int doubleRooms, int luxuryRooms, string sss)
+        {
+            Name = name;
+            DoubleRoomsCount = doubleRooms;
+            LuxuryRoomsCount = luxuryRooms;
+            Class = sss;
+        }
+
+        public string Name { get; private set; }
+        public int DoubleRoomsCount { get; private set; }
+        public int LuxuryRoomsCount { get; private set; }
+        public string Class { get; private set; }
+        public abstract string MyData();
+        public abstract bool CheckAvaulability(DateTime startDate, DateTime endDate, string roomtype, int totalGuests);
+        public abstract void Reserve(People person);
+        public abstract string ReservationsList();
+        public abstract decimal Payment(People person, Room room);
+        public abstract string CheckSeason(People person);
+        public abstract string Cancelreservation(string name);
+
+
+
+
+    }
+}
